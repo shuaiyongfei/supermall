6b5a5e866d42ecaf414ee2e087941b427ce7a1d6
shuaiyongfei <2840925348@qq.com>
2020-10-15 16:22:20 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="nav">
+  <button @click="btnclick">按钮</button>
+  <router-view />
+</div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    btnclick() {
+      this.$router.push('/about')
+    }
+  },
+}
+</script>
